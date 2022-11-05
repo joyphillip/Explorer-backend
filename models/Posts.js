0811b529fb47cf1d4845c8b6efcbe0f4bf02830c
JPhillip018 <jphillip018@gmail.com>
2022-11-04 20:33:10 -0400

    created user schema

--- models/Posts.js
+++ models/Posts.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose')
 
 const postsSchema = new mongoose.Schema({
-title: {
-    type: String,
-    required: true
-},
-country: {
-    type: String
-},
-description: {
-    type: String
-},
-images: {
-    type: [String]
-},
-date: {
-    type: String
-},
-favorite: {
-    type: Boolean,
-    default: false
-},
+    title: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    images: {
+        type: String
+    },
+    date: {
+        type: String
+    },
+    favorite: {
+        type: Boolean,
+        default: false
+    }
 })
 
 /* our schecma -> complied -> model -> is the one that have access to all the methods */
