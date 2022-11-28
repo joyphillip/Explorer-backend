c2d7c8ad3c753375e5a1d955de2bbfdad2d15f57
JPhillip018 <jphillip018@gmail.com>
2022-11-28 14:14:35 -0500

    updated create in posts route

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -5,7 +5,7 @@ const ctrls = require('../controllers')
 
 postsRouter.get('/', ctrls.posts.getAllPosts)
 postsRouter.get('/:id', ctrls.posts.showPost)
-postsRouter.post('/', ctrls.posts.createPost)
+postsRouter.post('/create', ctrls.posts.createPost)
 postsRouter.put('/update/:id', ctrls.posts.updatePost)
 postsRouter.delete('/:id', ctrls.posts.deletePost)
 postsRouter.get('/user/:id', ctrls.posts.getByUserId)
